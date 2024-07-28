ea745aa99c7d96c06097f2a36c0957f40724251f
Bhawesh15 <169034549+enclavegaming2020@users.noreply.github.com>
2024-07-28 20:47:40 +0545

    V 1.1.7.1

--- iphotobooth/src/pages/Checkout/Checkout.jsx
+++ iphotobooth/src/pages/Checkout/Checkout.jsx
@@ -7,63 +7,93 @@ import { useNavigate } from 'react-router-dom'
 
 const Checkout = () => {
 
-  const { getTotalCartAmount,token,productDetail,url,cartItems } = useContext(StoreContext)
-  const [data,setData] = useState({
-    firstname:"",
-    lastname:"",
-    email:"",
-    Address:"",
-    Pincode:"",
-    Province:"",
-    City:"",
-    phone:""
+  const { getTotalCartAmount, token, productDetail, url, cartItems } = useContext(StoreContext)
+  const [user, setUser] = useState([])
+  const [data, setData] = useState({
+    firstname: "",
+    lastname: "",
+    email: "",
+    Address: "",
+    Pincode: "",
+    Province: "",
+    City: "",
+    phone: ""
   })
 
 
   const navigate = useNavigate()
 
-  useEffect(()=>{
-    if(!token){
+  const fetchList = async (token) => {
+    try {
+      const response = await axios.get(`${url}/api/user/profile`, { headers: { token } });
+      if (response.data.success) {
+        setUser(response.data.data || []);
+      } else {
+        console.error("Error fetching list:", response.data.message);
+        setUser([]);
+      }
+    } catch (error) {
+      console.error("Error fetching list", error);
+      setUser([]);
+    }
+  };
+
+  useEffect(() => {
+    if (!token) {
       navigate('/cart')
       toast.error("Sign In Required")
-    }else if(getTotalCartAmount()==0){
+    } else if (getTotalCartAmount() == 0) {
       navigate('/cart')
       toast.error("Cart is Empty")
+    } else if (user.length === 0) {
+      const fetch = async () => {
+        await fetchList(localStorage.getItem("token"));
+      };
+      fetch();
     }
-  },[token]
+  }, [token]
   )
 
-
-
-  const onChangeHandler = (event)=>{
+  const onChangeHandler = (event) => {
     const name = event.target.name
     const value = event.target.value
-    setData(data=>({...data,[name]:value}))
+    setData(data => ({ ...data, [name]: value }))
   }
 
-  const placeOrder = async (event)=>{
+  const placeOrder = async (event) => {
     event.preventDefault()
     let orderitems = [];
-    productDetail.map((item)=>{
-      if (cartItems[item._id]>0){
+    productDetail.map((item) => {
+      if (cartItems[item._id] > 0) {
         let itemInfo = item;
         itemInfo["quantity"] = cartItems[item._id]
         orderitems.push(itemInfo)
       }
     })
     let orderData = {
-      address:data,
-      items : orderitems,
-      amount:getTotalCartAmount()
+      address: data,
+      items: orderitems,
+      amount: getTotalCartAmount()
     }
-    let response = await axios.post(url+"/api/order/placeorder",orderData,{headers:{token}});
+    let response = await axios.post(url + "/api/order/placeorder", orderData, { headers: { token } });
     if (response.data.success) {
       toast(response.data.message)
       navigate('/order')
-    }else{
+    } else {
       toast.error(response.data.message)
     }
   }
+  useEffect(() => {
+    if (user.length > 0) {
+      setData({
+        ...data,
+        firstname: user[0].firstname,
+        lastname: user[0].lastname,
+        email: user[0].email
+      })
+    }
+  }, [user])
+
 
   return (
     <div>
@@ -75,19 +105,19 @@ const Checkout = () => {
           <form onSubmit={placeOrder}>
             <div className="cust-name">
               <input name="firstname" value={data.firstname} onChange={onChangeHandler} type="text" placeholder='First Name' required />
-              <input name="lastname" value={data.lastname} onChange={onChangeHandler} type="text" placeholder='Last Name' required/>
+              <input name="lastname" value={data.lastname} onChange={onChangeHandler} type="text" placeholder='Last Name' required />
             </div>
             <div className="cust-phoneno">
-              <input name="phone" value={data.phone} onChange={onChangeHandler} type="tel" placeholder='Phone-Number' required  />
+              <input name="phone" value={data.phone} onChange={onChangeHandler} type="tel" placeholder='Phone-Number' required />
             </div>
             <div className="cust-email">
-              <input name="email" value={data.email} onChange={onChangeHandler} type="email" placeholder='Email' required/>
+              <input name="email" value={data.email} onChange={onChangeHandler} type="email" placeholder='Email' required />
             </div>
             <div className="cust-address">
-              <input name="Address" value={data.Address} onChange={onChangeHandler} type="text" placeholder='Address' required/>
-              <input name="Pincode" value={data.Pincode} onChange={onChangeHandler} type="text" placeholder='Pincode' required/>
-              <input name="Province" value={data.Province} onChange={onChangeHandler} type="text" placeholder='Province'required />
-              <input name="City" value={data.City} onChange={onChangeHandler} type="text" placeholder='City'required />
+              <input name="Address" value={data.Address} onChange={onChangeHandler} type="text" placeholder='Address' required />
+              <input name="Pincode" value={data.Pincode} onChange={onChangeHandler} type="text" placeholder='Pincode' required />
+              <input name="Province" value={data.Province} onChange={onChangeHandler} type="text" placeholder='Province' required />
+              <input name="City" value={data.City} onChange={onChangeHandler} type="text" placeholder='City' required />
             </div>
             <button type='submit'>Place Order</button>
           </form>
